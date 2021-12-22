b6c3c9c5a460ef91b3e5ff39bd713c347a106643
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-12-22 09:27:41 +0600

    Control display shows list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './style.css';
 import url from './images/logo.png';
+import shows from './shows.js';
+import showsView from './showsView.js';
+
+const displayShows = async () => {
+  const showsList = await shows.getShows();
+  showsView.displayShows(showsList);
+};
 
 window.onload = () => {
   document.querySelector('.logo-img').src = url;
+  displayShows();
 };
